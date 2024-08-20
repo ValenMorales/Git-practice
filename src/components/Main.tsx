b4c41379a82254be914c7ee6dc2bebbe719f99d5
ValenMorales <valentina.moraless@autonoma.edu.co>
2024-08-20 17:34:21 -0500

    products category section style implemented

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,17 @@ import "../styles/Main.css";
 export default function Main() {
   return (
     <main className="main">
-      <aside className="main__aside">aside</aside>
+      <aside className="main__aside">
+        <h2 className="categories-title">Select your favorite category</h2>
+        <ul className="categories-list">
+          <li className="item-list">Mazda</li>
+          <li className="item-list">Susuki</li>
+          <li className="item-list">Chevrolet</li>
+          <li className="item-list">Ford</li>
+          <li className="item-list">Honda</li>
+          <li className="item-list">Toyota</li>
+        </ul>
+      </aside>
       <section className="main__products">content for section</section>
     </main>
   );
