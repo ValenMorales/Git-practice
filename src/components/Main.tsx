b30e8b69c8e7a2e0e723c2d7257efeea0291fb9c
Pablo Gonzalez <pgosorio13@gmail.com>
2024-08-27 14:29:16 -0500

    Styles changes on Main content, aside content and footer styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -25,15 +25,15 @@ export default function Main() {
             <Card className="car-img" img="src/assets/pexels-bob-ronald-334686276-14832852.jpg"/>
             <Card className="car-img" img="src/assets/pexels-bertellifotografia-3007436.jpg"/>
           </div>
+          <section className="brand-section">
+            <Brand img="src/assets/peugeot-300x211.png" name="Peugeot"></Brand>
+            <Brand img="src/assets/Renault-300x300.png" name="Renault"></Brand>
+            <Brand img="src/assets/Seat-300x243.png" name="Seat"></Brand>
+            <Brand img="src/assets/Ferrari-300x300.png" name="Ferrari"></Brand>
+            <Brand img="src/assets/Lamborghini-263x300.png" name="Lamborghini"></Brand>
+            <Brand img="src/assets/Subaru-300x175.png" name="Subaru"></Brand>
+          </section>
         </section>
-      </section>
-      <section className="brand-section">
-        <Brand img="src/assets/peugeot-300x211.png" name="Peugeot"></Brand>
-        <Brand img="src/assets/Renault-300x300.png" name="Renault"></Brand>
-        <Brand img="src/assets/Seat-300x243.png" name="Seat"></Brand>
-        <Brand img="src/assets/Ferrari-300x300.png" name="Ferrari"></Brand>
-        <Brand img="src/assets/Lamborghini-263x300.png" name="Lamborghini"></Brand>
-        <Brand img="src/assets/Subaru-300x175.png" name="Subaru"></Brand>
       </section>
     </main>
   );
